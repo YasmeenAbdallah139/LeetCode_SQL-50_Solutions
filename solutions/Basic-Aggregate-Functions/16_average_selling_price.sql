effd8b1f026a96d5119fbb0e43a73156550105dc
YasmeenAbdallah139 <144189414+YasmeenAbdallah139@users.noreply.github.com>
2026-02-26 14:11:32 +0200

    Delete solutions/Basic-Aggregate-Functions directory

--- solutions/Basic-Aggregate-Functions/16_average_selling_price.sql
+++
@@ -1,9 +0,0 @@
-select p.product_id , round(
-    ISNULL(sum(p.price * u.units) * 1.0 / sum(u.units),0),
-    2
-    ) as average_price from
-Prices p
-LEFT JOIN
-UnitsSold u
-on p.product_id=u.product_id and purchase_date between start_date and end_date  
-group by p.product_id 
--- solutions/Basic-Aggregate-Functions/17_project_employees_i.sql
+++
@@ -1,6 +0,0 @@
-select p.project_id, round(avg(experience_years*1.0),2) as average_years from 
-Project p
-inner join
-Employee e
-on p.Employee_id = e.Employee_id
-group by project_id
