ac07dbddda07fa28d5181743f9448b44adbd4c09
YasmeenAbdallah139 <144189414+YasmeenAbdallah139@users.noreply.github.com>
2026-02-27 00:41:05 +0200

    Add SQL query for monthly transaction aggregation

---
+++ solutions/basic-aggregate-functions/20_monthly_transactions_i.sql
@@ -0,0 +1,14 @@
+SELECT CONVERT(VARCHAR(7), trans_date, 23) AS month, 
+       country, 
+       COUNT(*) AS trans_count,
+       SUM(CASE
+           WHEN state = 'approved' THEN 1
+           ELSE 0 
+       END) AS approved_count,
+       SUM(amount) AS trans_total_amount, 
+       SUM(CASE 
+           WHEN state = 'approved' THEN amount
+           ELSE 0
+       END) AS approved_total_amount 
+FROM Transactions 
+GROUP BY CONVERT(VARCHAR(7), trans_date, 23), country
