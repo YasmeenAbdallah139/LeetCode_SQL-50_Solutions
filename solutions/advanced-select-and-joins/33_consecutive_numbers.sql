c40b077270fb0c34059e1d5548c5d90c3664d0b5
YasmeenAbdallah139 <144189414+YasmeenAbdallah139@users.noreply.github.com>
2026-03-02 17:34:56 +0200

    Implement solutions for consecutive numbers query
    Added two solutions for finding consecutive numbers using self join and window functions.

--- solutions/advanced-select-and-joins/33_consecutive_numbers.sql
+++ solutions/advanced-select-and-joins/33_consecutive_numbers.sql
@@ -1,3 +1,5 @@
+--Solution using self join
+
 SELECT DISTINCT l1.num AS ConsecutiveNums 
 FROM 
 Logs l1
@@ -8,3 +10,17 @@ INNER JOIN
 Logs l3
 ON l2.id+1=l3.id
 WHERE l1.num=l2.num AND l2.num=l3.num
+
+
+  
+--Another solution using window functions
+  
+SELECT DISTINCT num AS ConsecutiveNums
+FROM
+(
+    SELECT *,
+        LAG(num) OVER(ORDER BY id) AS PREV,
+        LEAD(num) OVER(ORDER BY id) AS NEXT
+    FROM Logs 
+) AS t
+WHERE num = Next AND num = Prev
