ff881853e2b2f2c8b45a803b4ec36d13b7b6dc13
YasmeenAbdallah139 <144189414+YasmeenAbdallah139@users.noreply.github.com>
2026-04-18 00:18:32 +0200

    Update 22_game_play_analysis_iv.sql

--- solutions/basic-aggregate-functions/22_game_play_analysis_iv.sql
+++ solutions/basic-aggregate-functions/22_game_play_analysis_iv.sql
@@ -6,3 +6,21 @@ INNER JOIN (SELECT MIN(event_date) AS date,
             GROUP BY player_id) AS B
 ON A.player_id = B.player_id 
 AND DATEDIFF(day, B.date, A.event_date) = 1
+
+//another solution
+SELECT 
+    ROUND(
+        COUNT(DISTINCT t.player_id) * 1.0 /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+    2) AS fraction
+FROM
+(
+    SELECT 
+        player_id,
+        event_date,
+        LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS next_day,
+        ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn
+    FROM Activity
+) t
+WHERE t.rn = 1
+  AND DATEDIFF(day, t.event_date, t.next_day) = 1;
